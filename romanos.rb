172ef9cac808ddaa245517d08ee014cb4568f9da
Alex Quezada <alecs.alecs15@gmail.com>
2011-12-05 21:16:18 -0500

    test 1

--- romanos.rb
+++ romanos.rb
@@ -12,17 +12,10 @@ class RomanoTest < ActiveSupport::TestCase
   def test_1
 	assert_equal "I", @romanos.convertir(1)
   end
-
-   def test_2
-	assert_equal "II", @romanos.convertir(2)
-  end
 end
 
 class Romanos
 	def convertir(numero)
-		if numero = 2
-		"II"
-		else
-		"I"	
+			"I"	
 	end
 end
